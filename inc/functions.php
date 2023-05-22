c94e0b3478b2d89bcfe6d429025ec3e4c6a86c52
JohnFuglu <kuntz.romain@gmail.com>
2023-05-22 21:35:36 +0200

    AHHHHHH

---
+++ inc/functions.php
@@ -0,0 +1,127 @@
+<?php
+session_start();
+//variables de session
+$extension;
+$chemin;
+$nomFichier;
+//TODO si le nom du fichier est présent dans resized
+//avec  low, miniature, high. Retourner celui qui correspond
+//sinon faire la procédure actuelle de resize
+function fileExists(string $dir):bool{
+    $dirOriginals= scandir($dir);
+        if($dirOriginals){//si il y a qqchose dans le dossier
+            foreach($dirOriginals as $f){
+                if(str_contains($f, $_SESSION['nomFichier'])){
+                    return true; 
+                }
+                else{
+                    echo "fichier non présent dans ".$dir."!";
+                }
+            }
+            return false;
+        }   
+    }
+   function fileIsResized(string $dir,string $type):bool{
+    $dirRes= scandir($dir);
+        if($dirRes){//si il y a qqchose dans le dossier
+            foreach($dirRes as $f){
+                if(str_contains($f, $_SESSION['nomFichier'])){
+                    if(str_contains($f,$type)){
+                       $_SESSION['resizedImage']=RESIZE_DIR.$f;
+                       return true;
+                    }
+                }
+                else{
+                    echo "fichier non présent dans ".$dir."!";
+                }
+            }
+            return false;
+        }   
+    }
+function fileNameValidation($st):bool{
+    //ici check sur présence de low, high, mini
+    $test=explode('.', $st);
+    $t1= filter_var($test[0],FILTER_SANITIZE_SPECIAL_CHARS);
+    $t2=$test[1];
+    $_SESSION['nomFichier']=$t1;
+    return checkExtension($t2);
+}
+function checkExtension($st):bool{
+    strtolower($st);
+            switch ($st){
+            case 'png':
+                    $_SESSION['extension']=$st;
+                    return true;
+               
+            case 'jpeg':
+                     $_SESSION['extension']=$st;
+                    return true;
+              
+            case 'jpg':
+                    $_SESSION['extension']=$st;
+                    return true;
+             
+            case 'gif':
+                     $_SESSION['extension']=$st;
+                    return true;
+              
+            default:
+                return false;
+            }
+}
+//function FileInfo pour vérif image ...
+function getImageName($data):string {
+    $tmp= explode($data);
+    return count($tmp-1); 
+}
+function resizeWidth(int $width,image $img){
+    $perc=($width/$img->getWidth())*100;
+    $h=($img->getHeight()*$perc)/100;
+    resize([$width,$h],$img->getSource(),$img);
+}
+function resizeHeight(int $height,image $img){
+    $perc=($height/$img->getHeight())*100;
+    $w=($img->getWidth()*$perc)/100;
+    echo '<pre>';echo var_dump($img); echo '</pre>';
+    resize([$w,$height],$img->getSource(),$img);
+}
+function getSize($path, image $img){
+    $path='/opt/lampp/htdocs/PhpProject1/assets/originals/Hugo.jpg';
+    list($width,$height)= getimagesize(realpath($path));
+    $img->setHeight($height);
+    $img->setWidth($width);
+    $img->setSource($path);
+}
+
+function homothetie(string $path,float $rapport,image $img){
+    if($img->getResolution()=='mini'){
+            $vals=[($img->getWidth()*$rapport)/100,($img->getHeight()*$rapport)/100];
+    }else {
+    $vals=[$img->getWidth()*$rapport,$img->getHeight()*$rapport];}
+    resize($vals,$img->getSource(),$img);
+}
+/*TODO ici faire une fonction setFile qui remplace $newfile= etc
+*/
+function resize(array $hAndw,string $path,image $img){
+    $original=  imagecreatefromjpeg($path);
+    $destImage = imagecreatetruecolor($hAndw[0], $hAndw[1]);
+    imagecopyresampled($destImage, $original,0,0,0,0,$hAndw[0], $hAndw[1],$img->getWidth(),$img->getHeight());
+    $newfile= RESIZE_DIR.$img->getResolution().'_'.$img->getName().'.'.$img->getExtension();
+    imagejpeg($destImage,$newfile);
+    $_SESSION['newFile']= $newfile;
+    imagedestroy($destImage);
+    imagedestroy($original);
+}
+function resizeWitdhAndHeight(int $newHeight,int $newWidth, string $path,image $img){
+    
+    $original=  imagecreatefromjpeg($path);
+    list($width,$height)= getimagesize($path);
+    $destImage = imagecreatetruecolor($newWidth, $newHeight);
+    imagecopyresampled($destImage, $original,0,0,0,0,$newWidth, $newHeight,$img->getWidth(),$img->getHeight());
+    $newfile= RESIZE_DIR.$img->getResolution().'_'.$img->getName().'.'.$img->getExtension();
+    imagejpeg($destImage,$newfile);
+    $_SESSION['newFile']= $newfile;
+    imagedestroy($destImage);
+    imagedestroy($original);
+}
+?>
---
+++ resize.php
@@ -0,0 +1,61 @@
+<?php
+/*variables de chemins*/
+define('ASSETS_DIR', '/opt/lampp/htdocs/Projet1/assets');
+define('ORIGINALS_DIR', '/opt/lampp/htdocs/Projet1/Assets/originals');
+define('RESIZE_DIR','/opt/lampp/htdocs/Projet1/Assets/resized');
+define('INC_DIR','/opt/lampp/htdocs/Projet1/inc/');
+define('EMPTY_IMG',ASSETS_DIR.'/empty.png');
+require '/opt/lampp/htdocs/Projet1/inc/functions.php';
+//init des variablles 
+$image=EMPTY_IMG;
+$trouve=false;
+//récup des infos du html
+$nom=(isset($_POST['nom_image']) ? $_POST['nom_image']:'');
+
+
+if(isset($_POST['submit'])){
+//si le nom de fichier contient des infos normales
+
+    if(fileNameValidation($nom)){
+        // le fichier existe-t-il?
+        $trouve=fileExists(ORIGINALS_DIR);
+            if($trouve){
+                if(fileIsResized(RESIZE_DIR,"low")){//TODO faire avec form et post
+                    $image=$_SESSION['resizedImage'];
+                }
+            }
+        }
+    }
+?>
+
+<!doctype html>
+<html lang="fr">
+    <head>
+        <title>Service de redimension</title>
+        <meta charset="utf-8">
+        <link rel='stylesheet' href="inc/styleSheet.css">
+    </head>
+    <body>
+        <h1>Service de redimension d'images</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+            <fieldset>
+                <p>
+                    <label for="nom_image">Chercher une image</label>
+                    <input type="text" name="nom_image"id="nom_image" required="Entrez un nom d'image">
+                    <input type="submit" name="submit" value="chercher">
+                </p>
+                <p> 
+                    <label for="image">Image à redimensioner:</label>
+                    <img src="<?=$image?>" alt="image chargée" id="image">
+                </p>
+            </fieldset>
+            <fieldset>
+                <label for="height">Hauteur:</label>
+                <input type="number" name="height" id="height">
+                <label for="width">Largeur:</label>
+                <input type="number" name="width" id="width">
+            </fieldset>
+        </form>
+    </body>
+</html>
+
